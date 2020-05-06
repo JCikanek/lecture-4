318b9c9c99fd75c9c483e74c42ac536e703f6f97
Cigi <j.cikanek@gmail.com>
2020-05-06 16:56:57 +0200

    posledni zmeny

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const shoppingList = document.querySelector("#shoppingList");
 const deleteList = document.querySelector("#deleteItemList");
 
 function createShoppingItem(value) {
-    const p = document.createElement("p");
-    shoppingList.appendChild(p);
-    p.innerText = value;
+//    const p = document.createElement("p");
+//    shoppingList.appendChild(p);
+//    p.innerText = value;
+    shoppingList.innerHTML += `<p>${value}</p>`
+
 }
 
 function createDeleteItem(id) {
@@ -23,3 +25,27 @@ function clearDeleteList() {
     deleteList.innerHTML = ''
 }
 
+let seznam = [];
+
+
+const btnAdd = document.querySelector("#btnAdd");
+btnAdd.addEventListener("click",pridejPolozku);
+
+
+
+
+
+function pridejPolozku(){
+    const input = document.querySelector("#newItem");
+    const value = input.value;
+    
+
+
+    const polozka = {id:seznam.length,nazev:value};
+
+    seznam.push(polozka);
+    console.table(seznam);
+    
+    createShoppingItem(value)
+    
+}
