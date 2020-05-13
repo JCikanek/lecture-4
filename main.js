f2274aba0980c346decee23bcb0d27ff77254277
Cigi <j.cikanek@gmail.com>
2020-05-13 17:34:18 +0200

    Kod z 6.5

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const shoppingList = document.querySelector("#shoppingList");
 const deleteList = document.querySelector("#deleteItemList");
 
 function createShoppingItem(value) {
-//    const p = document.createElement("p");
-//    shoppingList.appendChild(p);
-//    p.innerText = value;
+    //    const p = document.createElement("p");
+    //    shoppingList.appendChild(p);
+    //    p.innerText = value;
     shoppingList.innerHTML += `<p>${value}</p>`
 
 }
@@ -25,27 +25,48 @@ function clearDeleteList() {
     deleteList.innerHTML = ''
 }
 
-let seznam = [];
+let seznam = [
+    { id: 0, nazev: "Banan" ,koupe: ["Patek", "Nedele", "Pondeli"] },
+    { id: 1, nazev: "Ananas",koupe: [ "Pondeli"]  },
+    { id: 2, nazev: "Meloun" },
+    { id: 5, nazev: "Hruska" }
+];
+
+let koupe = [
+    { id: 0, id_seznam:0, nazev: "Patek" },
+    { id: 2, id_seznam:0, nazev: "Nedele" },
+    { id: 1, id_seznam:1, nazev: "Pondeli" },
+];
+
 
 
 const btnAdd = document.querySelector("#btnAdd");
-btnAdd.addEventListener("click",pridejPolozku);
+btnAdd.addEventListener("click", pridejPolozku);
+
+
+function nejvyssiId() {
+    let aktualneNejvyssiId = 0;
+    for (const polozka of seznam) {
+        if (aktualneNejvyssiId < polozka.id) {
+            aktualneNejvyssiId = polozka.id;
+        }
+    }
+    return aktualneNejvyssiId;
+}
 
 
 
-
-
-function pridejPolozku(){
+function pridejPolozku() {
     const input = document.querySelector("#newItem");
     const value = input.value;
-    
 
 
-    const polozka = {id:seznam.length,nazev:value};
+
+    const polozka = { id: nejvyssiId() + 1, nazev: value };
 
     seznam.push(polozka);
     console.table(seznam);
-    
+
     createShoppingItem(value)
-    
+
 }
